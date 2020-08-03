2b0d3fb06329e92f4cd5e3e7bf291b0600c9a0ac
Ted M. Young <tedyoung@gmail.com>
2020-08-03 14:46:59 -0700

    Added popup marker locations using inlined JavaScript generated by Thymeleaf.

--- src/main/java/com/tedmyoung/tomtom/MapController.java
+++ src/main/java/com/tedmyoung/tomtom/MapController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class MapController {
 
@@ -14,7 +16,34 @@ public class MapController {
   @GetMapping("/")
   public String homePage(Model model) {
     model.addAttribute("apikey", tomTomApiKey);
+    model.addAttribute("coolLocations", coolLocations());
     return "home";
+  }
+
+  private List<Location> coolLocations() {
+    return List.of(
+        new Location(new double[]{-121.901481, 36.618253}, "Monterey Bay Aquarium"),
+        new Location(new double[]{21.006010, 52.231606}, "Palace of Culture and Science"),
+        new Location(new double[]{2.337612, 48.860717}, "Louvre Museum")
+    );
+  }
+
+  private static class Location {
+    private final double[] lnglat;
+    private final String description;
+
+    public Location(double[] lnglat, String description) {
+      this.lnglat = lnglat;
+      this.description = description;
+    }
+
+    public double[] getLnglat() {
+      return lnglat;
+    }
+
+    public String getDescription() {
+      return description;
+    }
   }
 
 }
